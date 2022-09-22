2abb2d7c7843771aee466454299255f1b2aab78f
benjamin hill <benjaminhill01@gmail.com>
2022-09-22 21:21:45 +1000

    Asp.net core web app setup The setup of the basic asp.net core web application

---
+++ BHWebShowcase/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BHWebShowcase.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
